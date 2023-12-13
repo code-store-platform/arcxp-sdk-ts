34c3790acf78aeef7071e88d5423ae3065517f9a
Olena Sevastian <enelipa@gmail.com>
2023-12-13 16:18:26 +0200

    pass org env in redirect only, export redirect types

--- src/api/abstract-api.ts
+++ src/api/abstract-api.ts
@@ -4,7 +4,7 @@ import rateLimit, { RateLimitedAxiosInstance } from 'axios-rate-limit';
 import { ArcError } from './error';
 
 export type ArcAbstractAPIOptions = {
-  credentials: { organizationName: string; accessToken: string; orgSiteEnv?: string };
+  credentials: { organizationName: string; accessToken: string };
   apiPath: string;
   maxRPS?: number;
 };
--- src/api/redirect/index.ts
+++ src/api/redirect/index.ts
@@ -3,24 +3,25 @@ import { ArcRedirectRuleContentType, ArcRedirectRulesResponse, ArcRedirectRuleTy
 
 export class ArcRedirect extends ArcAbstractAPI {
   constructor(options: ArcAPIOptions) {
-    super({ ...options, apiPath: `delivery-api/v1/cloudlet/redirector/site/${options.credentials.orgSiteEnv}` });
+    super({ ...options, apiPath: `delivery-api/v1/cloudlet/redirector/site` });
   }
 
-  async getRedirects(): Promise<ArcRedirectRulesResponse> {
-    const { data } = await this.client.get(`/rule?limit=1000`);
+  async getRedirects(orgSiteEnv: string): Promise<ArcRedirectRulesResponse> {
+    const { data } = await this.client.get(`/${orgSiteEnv}/rule?limit=1000`);
     return data;
   }
 
   async updateArcRedirectRule(
+    orgSiteEnv: string,
     redirectID: string,
     redirectRuleContent: ArcRedirectRuleContentType
   ): Promise<ArcRedirectRuleType> {
-    const { data } = await this.client.put(`/rule/${redirectID}`, redirectRuleContent);
+    const { data } = await this.client.put(`/${orgSiteEnv}/rule/${redirectID}`, redirectRuleContent);
     return data;
   }
 
-  async activateRedirectsVersion(): Promise<{ task_id: string }> {
-    const { data } = await this.client.post(`/version`);
+  async activateRedirectsVersion(orgSiteEnv: string): Promise<{ task_id: string }> {
+    const { data } = await this.client.post(`/${orgSiteEnv}/version`);
     return data;
   }
 }
