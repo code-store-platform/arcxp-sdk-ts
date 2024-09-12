73f98d43dfe180b6fb1296ad675bb9ed080751f1
Max <msadovyi404@gmail.com>
2024-09-12 11:03:03 +0300

    fix test

--- src/tests/api/basic.test.ts
+++ src/tests/api/basic.test.ts
@@ -10,6 +10,11 @@ describe('Arc API', () => {
   test('Should throw authorization error (Invalid token)', async () => {
     const API = ArcAPI({ credentials: { organizationName: 'codestore', accessToken: Date.now().toString() } });
 
-    await expect(API.Draft.generateId(Date.now().toString())).rejects.toThrowError(/401 Authorization Required/);
+    await expect(API.Draft.generateId(Date.now().toString())).rejects.toMatchObject({
+      name: 'ArcDraftError',
+      message: 'Request failed with status code 401',
+      service: 'ArcDraft',
+      responseStatus: 401,
+    });
   });
 });
