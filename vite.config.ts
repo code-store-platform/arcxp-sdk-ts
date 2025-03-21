9bada85b8b44600b3f39bfc1b1998d05f6eaddca
Max <msadovyi404@gmail.com>
2025-03-21 12:27:41 +0200

    update vitest

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     reporters: ['verbose'],
+    snapshotFormat: {
+      printBasicPrototype: false,
+    },
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'v8', // or 'v8'
+    },
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
   },
